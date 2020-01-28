e44bda7c1fd9e89c5ff1062df3ba43beb964df4e
jessica <55108619+jess-ica@users.noreply.github.com>
2020-01-28 00:28:16 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// prints "hi" in the browser's dev tools console
+console.log("hi");
+
+$(function(){
+  $("#travel-webpage").load("travel-webpage.html"); 
+});
+
+$(function(){
+  $("#recipe-book").load("recipe-book.html"); 
+});
+
+$(function(){
+  $("#blog").load("blog.html"); 
+});
